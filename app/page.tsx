8395194e8dcb7b25206a4f39e97b22e18223fe21
Callme-VR <176268004+Callme-VR@users.noreply.github.com>
2025-07-31 22:03:31 +0530

    make new Login Component with framer motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import GlowMenu from "./components/glowmenu";
 import Sidebar from "./components/sidebar";
 import StarBorder from "./components/starborder";
+import Login from "./components/login";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black text-white flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold mb-8">Glowing Navigation Menu</h1>
       <GlowMenu />
       {/* <Sidebar /> */}
-      <StarBorder children={undefined} />
+      {/* <StarBorder children={undefined} /> */}
+      <Login />
     </main>
   );
 }
